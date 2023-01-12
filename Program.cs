153453c8a36f2fb089ad0d3ca592c286b5550329
Acrobaticbag3 <kevintom214@gmail.com>
2023-01-12 12:31:06 +0100

    it begins

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data_Structures_Task_One {
+    
+    internal class program {
+
+        public void Main(string[] args) {
+            
+        }
+    }
+}
