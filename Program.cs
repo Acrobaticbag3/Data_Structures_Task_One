5da581e3dd37915a59328ed63b6236ff8094be98
Acrobaticbag3 <kevintom214@gmail.com>
2023-01-19 12:28:29 +0100

    scheees

--- Program.cs
+++ Program.cs
@@ -7,49 +7,36 @@ namespace Data_Structures_Task_One {
     internal class program {
 
         public static void Main(string[] args) {
-            string exp = "[()]{}{[()()]()}";
-        }
+            string input = "[()]{}{[()()]()}";
+            Stack<char> stack = new Stack<char>();
 
-        public class BalancedBrackets {
-            public class Stack {
-                public int top = -1;                    // Used to set the new top of our stack
-                public char[] items = new char[100];    // Used to track stack max 
-                
-                public void Push(char x) {              // Adds char if there is space
-
-                    if (top == 99) {
-                        Console.WriteLine("Stack full");    
-                    } else {
-                        items[++top] = x;       // Increase by one and set x to value
+            foreach (char c in input) {
+                if (input == "[" || input == "(" || input == "{") {
+                    stack.Push(c);   
+                } else {
+                    
+                    if(stack.Count == 0){
+                        Console.WriteLine("Input is unbalanced");
+                        return;
                     }
+                    char popedChar = stack.Pop();
                 }
 
-                char Pop() {
-
-                    if (top == -1) {
-                        Console.WriteLine("Underflow error");
-                        return '\0';
-                    } else {
-                        char element = items[top];
-                        top--;
-                        return element;
-                    }
+                bool check = false;
+                if (c == ')') {
+                    check = true;  
+                } else if (c == '}') {
+                    check = true;
+                } else if (c == ']') {
+                    check = true;
+                } else {
+                    check = false;
                 }
 
-                Boolean isEmpty() {
-                    return (top == -1) ? true : false;
-                }
-
-                //  Returns true if character1 and character2 are matching left and right brackets
-                static Boolean isMatchingPair(char character1, char character2) {   
-                    if (character1 == '(' && character2 == ')')
-                        return true;
-                    else if (character1 == '{' && character2 == '}')
-                        return true;
-                    else if (character1 == '[' && character2 == ']')
-                        return true;
-                    else 
-                        return false;
+                if(stack.Count == 0) {
+                    Console.WriteLine("Input is balanced");
+                } else {
+                    Console.WriteLine("Input is not balanced");
                 }
             }
         }
